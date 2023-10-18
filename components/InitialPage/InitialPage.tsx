d5513ea86791113e221e3c582e596f8b9092df87
Joao-Ortega <joao_ortega17@hotmail.com>
2023-10-18 18:43:32 -0300

    Styling buttons main page

--- components/InitialPage/InitialPage.tsx
+++ components/InitialPage/InitialPage.tsx
@@ -1,11 +1,31 @@
 'use client'
-import { Box, Button, TextField } from '@mui/material'
+import { Box, Button, CircularProgress, TextField } from '@mui/material'
 import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 
 export default function InitialPage() {
   const [isCLicked, setIsClicked] = useState<boolean>(false);
+  const [buttonLoading, setButtonLoading] = useState<boolean>(false);
+  const [isDisable, setIsDisable] = useState<boolean>(true);
   const router = useRouter();
+
+  useEffect(() => {
+    if (buttonLoading) {
+      router.push('/game')
+    }
+  }, [buttonLoading]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    if (e.target.value.trim().toUpperCase() === 'OKIKUKAI') {
+      setIsDisable(false);
+    } else {
+      setIsDisable(true);
+    }
+  }
+
+  const handleKotoSimonGame = () => {
+    
+  }
 
   return (
     <Box
@@ -22,25 +42,44 @@ export default function InitialPage() {
         height='15vh'
         justifyContent='space-evenly'
         width='55vw'
-        sx={{ border: '1px solid red' }}
+        // sx={{ border: '1px solid red' }}
       >
         <Button
           variant='contained'
-          onClick={() => router.push('/game')}
+          onClick={() => setButtonLoading(true)}
           sx={{
             backgroundColor: 'white',
             color: 'black',
-            '& .MuiButtonBase-root:active': {
-              backgroundColor: 'red'
+            '&.MuiButtonBase-root:active': {
+              backgroundColor: 'black'
+            },
+            '&.MuiButtonBase-root:hover': {
+              backgroundColor: 'black',
+              color: 'white'
             }
           }}
         >
-          Perguntas Gerais
+          {buttonLoading ?
+            <CircularProgress
+              sx={{ color: 'white' }}
+              size={25}
+            /> :
+            'Perguntas Gerais'
+          }
         </Button>
         <Button
           onClick={() => setIsClicked(!isCLicked)}
           variant='contained'
-          sx={{ backgroundColor: 'white', color: 'black' }}
+          sx={{
+            backgroundColor: isCLicked ? 'black' : 'white',
+            color: isCLicked ? 'red' : 'black',
+            // '&.MuiButtonBase-root:active': {
+            //   backgroundColor: 'black'
+            // },
+            '&.MuiButtonBase-root:hover': {
+              backgroundColor: 'white',
+            }
+          }}
         >
           Koto Simon
         </Button>
@@ -48,12 +87,39 @@ export default function InitialPage() {
       {isCLicked && (
         <Box
           sx={{ margin: "2% 0 0 0" }}
+          display='flex'
+          flexDirection='column'
         >
           <TextField
             placeholder='Digite a Senha'
             size='small'
+            onChange={handleChange}
             sx={{ backgroundColor: 'white', width: '55vw', borderRadius: '8px' }}
           />
+          <Button
+            variant='outlined'
+            disabled={isDisable}
+            onClick={handleKotoSimonGame}
+            sx={{
+              backgroundColor: 'black',
+              border: 'black',
+              color: 'white',
+              '&.MuiButtonBase-root:active': {
+                backgroundColor: 'black'
+              },
+              '&.MuiButtonBase-root:hover': {
+                backgroundColor: 'black',
+                color: 'white',
+                border: 'black'
+              },
+              '&.MuiButtonBase-root:disabled': {
+                backgroundColor: 'black',
+                color: 'black'
+              }
+            }}
+          >
+            Entrar
+          </Button>
         </Box>
       )}
     </Box>
