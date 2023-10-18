c17e52871548dda03565e7b9e4d8eb57510fbb07
Joao-Ortega <joao_ortega17@hotmail.com>
2023-10-18 09:12:03 -0300

    Adding initial page with button to choose

---
+++ components/InitialPage/InitialPage.tsx
@@ -0,0 +1,58 @@
+'use client'
+import { Box, Button, TextField } from '@mui/material'
+import React, { useState } from 'react'
+
+export default function InitialPage() {
+  const [isCLicked, setIsClicked] = useState<boolean>(false);
+
+  return (
+    <Box
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      flexDirection='column'
+      height='90vh'
+      width='100vw'
+    >
+      <Box
+        display='flex'
+        flexDirection='column'
+        height='15vh'
+        justifyContent='space-evenly'
+        width='55vw'
+        sx={{ border: '1px solid red' }}
+      >
+        <Button
+          variant='contained'
+          sx={{
+            backgroundColor: 'white',
+            color: 'black',
+            '& .MuiButtonBase-root:active': {
+              backgroundColor: 'red'
+            }
+          }}
+        >
+          Perguntas Gerais
+        </Button>
+        <Button
+          onClick={() => setIsClicked(!isCLicked)}
+          variant='contained'
+          sx={{ backgroundColor: 'white', color: 'black' }}
+        >
+          Koto Simon
+        </Button>
+      </Box>
+      {isCLicked && (
+        <Box
+          sx={{ margin: "2% 0 0 0" }}
+        >
+          <TextField
+            placeholder='Senha'
+            size='small'
+            sx={{ backgroundColor: 'white', width: '55vw', borderRadius: '8px' }}
+          />
+        </Box>
+      )}
+    </Box>
+  )
+}
